4ab783c80de66769898128475eecbf8c42f1874b
aford1 <aford1@my.hpu.edu>
2011-10-22 17:56:52 -1000

    added capability to retrieve site names and display on the popup

--- v1.4(on button)/QrataContentScript.js
+++ v1.4(on button)/QrataContentScript.js
@@ -1,21 +1,39 @@
 //Content script for Qrata Extension
 
+//When the dom finishes loading, run the showRatings function
+$(document).ready(function() {
+    showRatings();
+});
+
+//global variables to manage the status of the popup
+var onPopup = false;
+var onRating = false;
+var popupExists = false;
+var timeoutID;
 
 
+//***************************************************************************************************
+//This function shows rating icons for each google search result
 function showRatings() {
-
     console.log("showRatings called");
     
+    //get the result elements and store them in a variable
     var results = $('.vsc').get();
+    var sites = $('.l').get();
     console.log("results: "+results.length);
+    
    
-    //Create new html elements for our ratings and append them to the vsc classes
-    for(var i=0; i<results.length; i++){
-        var rating = $("<div/>", {
+    //Create new html elements for each rating and append them to the proper elements
+    for(var i=0; i<sites.length; i++){
+        console.log(sites[i].href);
+        $("<div/>", {
             "id": i,
             "class": "qRataRating",
-            //rating will go here*********************************************************************
-            text: "100"
+            
+            //********rating will go here********
+            text: 100
+            //**********************************
+            
         }).appendTo("#center_col");
         //get the position of this vsc element
         var vscPosition = $(results[i]).position();
@@ -39,29 +57,36 @@ function showRatings() {
    
     //give the elements some mousover event and mouse out events
     $(".qRataRating").mouseover(function(event){
-        qrataPopup(event.target.id);
+        console.log("mouseover event for Rating fired");
+        if (onPopup || onRating) {
+            return false;
+        }
+        onRating = true;
+        qrataPopup(event.target.id, sites[event.target.id].href);
+    }).mouseout(function(){
+        console.log("mousout event for Rating fired");
+        onRating = false;
+        timeoutID = setTimeout(leavePopup, 500);
+        console.log("onPopup: "+onPopup+"   onRating: "+onRating);
     });
-    //mouse has left the rating icon
-    $(".qRataRating").mouseout(function(){
-        leavePopup();
-    });
+
 }
 
-
-//When the dom finishes loading, run the showRatings function
-$(document).ready(function() {
-    showRatings();
-});
-
+//********************************************************************************************
 //this popup window appears when the user mouses over the qrata rating
-function qrataPopup(id){
+function qrataPopup(id, site){
+
+    if(popupExists){
+        leavePopup();
+    }
+
     $("#"+id+".qRataRating").css("background-color","lime");
     
     //create a new html element for our popup
     $("<div/>", {
         "class": "qrataPopup",
-        text: "QRata Rating for id: "+id
-    }).appendTo('body');
+        text: "QRata Rating for: "+site
+    }).mouseover(keepOpen).mouseout(letClose).appendTo('body');
     //add the css for the html element
     var position = $("#"+id+".qRataRating").position();
     $(".qrataPopup").css({
@@ -70,13 +95,33 @@ function qrataPopup(id){
         "width":"400px", 
         "color":"white",
         "position":"absolute",
-        "left":position.left+25,
+        "left":position.left+28,
         "top":position.top,
         "z-index":"5002"
     });
+    popupExists=true;
 }
-//when the users mouse leaves the rating, hide the popup
+
+//***********************************************************************************************
+//Functions to manage whether to keep the popup open or not
+function keepOpen() {
+    onPopup = true;
+    console.log("onPopup is true");
+}
+function letClose() {
+    onPopup = false;
+    timeoutID = setTimeout(leavePopup, 500);
+}
+//***********************************************************************************************
+//if the users mouse leaves the rating and the popup, then remove the popup
 function leavePopup(){
     $(".qRataRating").css("background-color","green");  
-    $(".qrataPopup").remove();
+    clearTimeout(timeoutID);
+    //if the popup exists and the users mouse isn't on the rating or the popup, then remove the popup
+    if (popupExists && !onRating && !onPopup) {
+        $(".qrataPopup").remove();
+        popupExists = false;
+    }
+    
 }
+//***********************************************************************************************
